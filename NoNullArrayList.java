6be02005a1b320c8c418190422bfff82c02ff749
OnikuSpedu <srakibnyc1@gmail.com>
2020-12-10 13:24:21 -0500

    Added methods to NoNullArrayList

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,9 +1,31 @@
 import java.util.ArrayList;
 class NoNullArrayList<T> extends ArrayList<T> {
-    public NoNullArrayList<T>() {
-
+    public NoNullArrayList() {
+        super();
     }
-    public NoNullArrayList<T>(int startingCapacity) {
-
+    public NoNullArrayList(int startingCapacity) {
+        super(startingCapacity);
+    }
+    @Override
+    public boolean add(T e) {
+        if(e == null) {
+            return false;
+        } else {
+            return super.add(e);
+        }
+    }
+    @Override
+    public void add(int index, T element) {
+        if(element != null) {
+        super.add(index, element);
+        }
+    }
+    @Override
+    public T set(int index, T element) {
+        if(element != null) {
+            return super.set(index, element);
+        } else {
+            return null;
+        }
     }
 }
