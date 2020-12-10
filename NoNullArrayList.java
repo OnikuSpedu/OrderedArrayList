64a283e7cd8079376dff610c605837eb35ebb08e
OnikuSpedu <srakibnyc1@gmail.com>
2020-12-10 13:13:50 -0500

    Added constructor

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,5 +1,9 @@
 import java.util.ArrayList;
-
 class NoNullArrayList<T> extends ArrayList<T> {
+    public NoNullArrayList<T>() {
 
+    }
+    public NoNullArrayList<T>(int startingCapacity) {
+
+    }
 }
